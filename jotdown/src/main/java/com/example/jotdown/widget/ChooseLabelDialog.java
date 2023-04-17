f3f789d1293d6ba95b94e7e93bf3c23cdcacf4af
NH-X <1709597119@qq.com>
2023-04-17 16:55:19 +0800

    2023/04/17 16:55

--- jotdown/src/main/java/com/example/jotdown/widget/ChooseLabelDialog.java
+++ jotdown/src/main/java/com/example/jotdown/widget/ChooseLabelDialog.java
@@ -1,8 +1,18 @@
 package com.example.jotdown.widget;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.view.View;
 
 public class ChooseLabelDialog implements View.OnClickListener {
+    private Dialog dialog;                  //声明一个对话框对象
+    private View view;                      //声明一个视图对象
+
+
+    public ChooseLabelDialog(Context context){
+
+    }
+
     @Override
     public void onClick(View view) {
 
