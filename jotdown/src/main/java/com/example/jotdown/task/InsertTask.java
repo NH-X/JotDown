9b241f3d3694a4e997e277f9f98e9e0b832de8a0
NH-X <1709597119@qq.com>
2023-06-01 23:31:05 +0800

    2023/06/01 23:30

---
+++ jotdown/src/main/java/com/example/jotdown/task/InsertTask.java
@@ -0,0 +1,12 @@
+package com.example.jotdown.task;
+
+import android.os.AsyncTask;
+
+import com.example.jotdown.bean.NodeInfo;
+
+public class InsertTask extends AsyncTask<NodeInfo,Void,Void> {
+    @Override
+    protected Void doInBackground(NodeInfo... nodeInfos) {
+        return null;
+    }
+}
---
+++ jotdown/src/main/java/com/example/jotdown/viewmodel/InsertViewModel.java
@@ -0,0 +1,20 @@
+package com.example.jotdown.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.jotdown.MainApplication;
+import com.example.jotdown.bean.Resource;
+import com.example.jotdown.db.NodesDBHelper;
+
+public class InsertViewModel extends ViewModel {
+    private static final String TAG="InsertViewModel";
+
+    private NodesDBHelper helper;                       //数据库帮助器
+
+    private MutableLiveData<Resource<Boolean>> mInsertSchedule=new MutableLiveData<>();
+
+    public void init(){
+        helper= MainApplication.getNodesDBHelper();
+    }
+}
